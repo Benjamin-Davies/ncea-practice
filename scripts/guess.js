7015c3ebe5ae93dc59e37d91f039c7f317cb5560
Benjamin Davies <bentendavies@gmail.com>
2019-02-14 11:34:57 +1300

    Add files via upload

---
+++ scripts/guess.js
@@ -0,0 +1,23 @@
+// When using this script, you must have the correct dom elements
+// and a global variable answer.
+
+(function() {
+  const guessForm = document.getElementById('guess-form');
+  const result = document.getElementById('result');
+
+  guessForm.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+
+    var value = this['guess'].value;
+    var correct = value === answer;
+
+    if (value)
+      result.innerText = 
+        `${
+          correct ? 'Correct' : 'Wrong'
+        }! This poem was written by a ${answer}.`;
+    else
+      result.innerText =
+        'Please make a guess...';
+  });
+})();
